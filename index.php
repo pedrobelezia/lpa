d113917e514ab2bdd65fc30f06247f42092033f3
root <pedrobelezia@hotmail.com>
2017-06-05 18:39:48 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bootstrap 3, from LayoutIt!</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Slab" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Bad+Script" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+
+
+
+
+
+
+    <!--BXSLIDER-->
+    <script src="bxslider/dist/jquery.bxslider.min.js"></script>
+    <link href="bxslider/dist/jquery.bxslider.min.css" rel="stylesheet" />
+     
+     <!--lightBox-->
+     <link href="node_modules/lightbox2/dist/css/lightbox.min.css" rel="stylesheet">
+     <script src="node_modules/lightbox2/dist/js/lightbox.min.js"></script>
+
+
+
+  </head>
+  <body>
+
+    <div class="container-fluid">
+  	   <div class="row">
+          <div class="col-md-12 topo">
+            <div class="infoTel" style="color:#CCA776"><i class="fa fa-phone" style="color:#000"></i> Telefones:  (31) 9 9208-2893  &nbsp;&nbsp; <i class="fa fa-clock-o" style="color:#000"></i> Estamos abertos: Seg. a Sex 09:00 - 18:00 </div> 
+            <div class="logo">
+             <img src="img/logo.jpg" width="120px">
+            </div>
+            
+          </div>
+          <div class="col-md-12 menu">
+            <div class="col-md-7 col-md-offset-3">
+                <?php include 'menu.php' ?>
+            </div>
+          </div>
+          <div class="col-md-12 banner">
+            <center><div class="faixa"></div></center>
+
+              <ul class="bxslider">
+
+                  <li><h3>Advogada</h3>
+                  <h1>Renata Abraão</h1></li>
+                   <li><h3>Teste</h3>
+                  <h1>Pedro Belezia</h1></li>
+
+              </ul>
+
+           
+            <button class="btnCaption">MAIS INFO</button>
+          </div>
+         
+          <div class="col-md-12 sobre">
+          <div class="container">
+
+              <center><div class="faixa"></div></center>
+
+           <h1>Sobre Nós</h1>
+     
+           <p>
+
+              Em suma, atuamos em temas relativos a obrigações de dar, fazer e não fazer; execuções de título executivo extrajudicial, como contratos, nota promissória, cheque, duplicata, letra de câmbio; cumprimento de sentenças, incluindo sentença condenatória penal, sentença arbitral e homologatória de acordo; ações de cobrança em geral e monitórias.
+
+           </p>
+           
+           </div>
+          </div>
+          
+           <div class="col-md-12 info">
+            <div class="container">
+              <div class="col-md-3">
+                <div class="direito"><span id="c">Cível</span><button class="mais" style="display: none">
+                Saiba Mais</button><br>
+              
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="direito"><span id="i">Imobiliário</span><button class="maisI" style="display: none">
+                Saiba Mais</button><br>
+              </div>
+              </div>
+              <div class="col-md-3">
+              <div class="direito"><span id="co">Consumidor</span><button class="maisCo" style="display: none">
+              Saiba Mais</button><br>
+           
+              </div>
+              </div>
+              <div class="col-md-3">
+              <div class="direito"><span id="e">Empresarial</span><button class="maisE" style="display: none">Saiba Mais
+              </button><br>
+             
+              </div>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="col-md-12 advogado">
+            <div class="container">
+            <div class="col-md-6">
+              <center><div class="faixa"></div></center>
+              <h1>Advogada</h1>
+              <b style="font-size:16px">Renata Lourenço Pereira Abrão</b><br><br>
+                Graduada em Direito pela PUC Minas.<br>
+                Pós Graduada Direito Civil pela PUC Minas. <br>
+                Especialista em Direito Imobiliário pela PUC Minas.<br>
+                Aperfeiçoamento em Direito do Consumidor pelo Instituto Consuma Seus Direitos.
+              </div>
+              <div class="col-md-6">
+                <img src="img/renata2.jpg" class="thumbnail" width="100%">
+              </div>
+            </div>
+          </div>
+
+               <div class="col-md-12 area"> 
+            <div class="container">
+              <center><div class="faixa"></div></center>
+              <h1>Áreas de Atuação</h1>
+              <div class="areas">
+                  <div class="civel">
+                    <a href="javascript:;" class="civelA">Cível</a>
+                  </div>
+                  <p class="civelP">
+                    
+                  <b>Contratos:</b> consultoria, atuando na elaboração, análise e negociação contratual, abrangendo contratos de todas as espécies cíveis e contratos empresariais, dentre os quais, compra e venda, locação, parceria, garantias, prestação de serviços e corretagem; contencioso, atuando judicialmente quanto à revisão, ajuste, execução e rescisão contratual.<br>
+
+                  <b>Família:</b> causas relativas a casamento, divórcio, dissolução de união estável, guarda de menor, pensão alimentícia, investigação e contestação de paternidade, anulação e retificação de registro.<br>
+
+                  <b>Sucessões:</b> inventários judiciais e extrajudiciais, partilha, elaboração e contestação de testamento.<br>
+
+                  <b>Direitos reais:</b> ações possessórias e relativas à propriedade, medidas liminares, usucapião.
+
+
+                  </p>
+                   <div class="civel">
+                    <a href="javascript:;" class="imoA">Direito Imobiliário</a>
+                  </div> 
+                  <p class="imoP">
+                    
+                  Consultoria e contencioso. Elaboração e análise de contratos. Assessoria a condomínios residenciais e comerciais, atuando no apoio à gestão administrativa e financeira do síndico e dos conselhos, incluindo prestação de contas, elaboração de regimento interno, convenção e balancetes, redação de atas, comunicados, notificações extrajudiciais e orçamentos, administração das despesas e receitas, cobranças extrajudiciais e judiciais de inadimplência, gerenciamento de conta bancária e emissão de boletos.
+
+                  </p>
+
+
+                  <div class="civel">
+                    <a href="javascript:;" class="conA">Direito do Consumidor</a>
+                  </div>
+                  <p class="conP">
+                  Causas relativas ao direito do consumidor em geral, atuando tanto em favor do consumidor quanto do fornecedor de serviços e produtos. Atuamos extrajudicialmente e em juízo no que se refere a contratos de consumo em geral, como bancários, de seguro, de prestação de serviços, de compra e venda, de transporte. Questões referentes a venda casada, juros abusivos, financiamento, práticas abusivas de consumo, vícios no produto e na prestação de serviços, contratos abusivos.
+                  </p>
+                  <div class="civel">
+                    <a href="javascript:;" class="emA">Direito do Empresarial</a>
+                  </div>
+                  <p class="emP">
+                    
+                    Consultoria e contencioso empresarial. Análise e elaboração de contratos; defesa do patrimônio da sociedade e dos sócios; recuperação judicial e extrajudicial; falência; direito da propriedade intelectual (marcas e patentes) e direitos autorais;
+                  </p>
+               
+                
+              </div>
+            </div>
+          </div>
+
+           <div class="col-md-12 galeria">
+            <div class="container">
+              <center><div class="faixa"></div></center>
+              <h1 style="color:#CCA776">Nosso Escritório</h1>
+              <div class="col-md-4">
+                <div class="fotoEsc">
+                  <a href="img/esc1.jpg" data-lightbox="fotos"><img src="img/esc1.jpg"></a>
+                </div>
+                </div>
+                <div class="col-md-4">
+                <div class="fotoEsc">
+                  <a href="img/esc2.jpg" data-lightbox="fotos"><img src="img/esc2.jpg"></a>
+                </div>
+                </div>
+                <div class="col-md-4">
+                <div class="fotoEsc">
+                  <a href="img/esc3.jpg" data-lightbox="fotos"><img src="img/esc3.jpg"></a>
+                </div>
+                </div>
+                <div class="col-md-4">
+                <div class="fotoEsc">
+                  <a href="img/esc4.jpg" data-lightbox="fotos"><img src="img/esc4.jpg"></a>
+                </div>
+                </div>
+                <div class="col-md-4">
+                <div class="fotoEsc">
+                  <a href="img/esc5.jpg" data-lightbox="fotos"><img src="img/esc5.jpg"></a>
+                </div>
+                </div>
+                <div class="col-md-4">
+                <div class="fotoEsc">
+                  <a href="img/esc6.jpg" data-lightbox="fotos"><img src="img/esc6.jpg"></a>
+                </div>
+                </div>
+            </div>
+          </div>
+
+
+
+          <div class="col-md-12 contato">
+            <div class="container">
+             <center><div class="faixa"></div></center>
+              <h1>Contato</h1>
+
+
+              <div class="col-md-6">
+                <form method="post" action="enviar.php">
+                <input type="text" name="nome" placeholder="Nome..." class="fContato">
+                <input type="text" name="email" placeholder="Email..." class="fContato">
+                <input type="text" name="telefone" placeholder="Telefone..." class="fContato">
+                <textarea class="mContato" placeholder="Mensagem..."></textarea>
+                <input type="submit" value="Enviar" class="btnEnviar">
+                </form>
+              </div>
+              <div class="col-md-6">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.534350314348!2d-43.95041148508515!3d-19.944018086595914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa69778cd147149%3A0x8299647563ebe99!2sAv.+Prudente+de+Morais%2C+901+-+202+-+Coracao+de+Jesus%2C+Belo+Horizonte+-+MG!5e0!3m2!1spt-BR!2sbr!4v1496695778066" width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+              </div>
+            </div>
+          </div>
+         
+
+  	   </div>
+    </div>
+    <script>
+        $(document).ready(function(){
+          $('.bxslider').bxSlider();
+
+          $("#c").mouseover(function(){
+            $(".mais").show("slow");
+            $("#c").hide("slow");
+          });
+          $(".mais").mouseout(function(){
+            $(".mais").hide("slow");
+            $("#c").show("slow");
+          });
+
+          $("#i").mouseover(function(){
+            $(".maisI").show("slow");
+            $("#i").hide("slow");
+          });
+          $(".maisI").mouseout(function(){
+            $(".maisI").hide("slow");
+            $("#i").show("slow");
+          });
+
+          $("#co").mouseover(function(){
+            $(".maisCo").show("slow");
+            $("#co").hide("slow");
+          });
+          $(".maisCo").mouseout(function(){
+            $(".maisCo").hide("slow");
+            $("#co").show("slow");
+          });
+
+          $("#e").mouseover(function(){
+            $(".maisE").show("slow");
+            $("#e").hide("slow");
+          });
+          $(".maisE").mouseout(function(){
+            $(".maisE").hide("slow");
+            $("#e").show("slow");
+          });
+
+          $(".civelA").click(function(){
+            $(".civelP").toggle("slow");
+          });
+          $(".imoA").click(function(){
+            $(".imoP").toggle("slow");
+          });
+          $(".conA").click(function(){
+            $(".conP").toggle("slow");
+          });
+           $(".emA").click(function(){
+            $(".emP").toggle("slow");
+          });
+
+
+
+        });
+    </script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,31 @@
+	<nav class="navbar navbar-default" role="navigation" style="background:transparent;border:none;text-align: center">
+				<div class="navbar-header">
+					 
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						 <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+					</button>
+				</div>
+				
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav" style="font-size:15px;margin-top:7px;text-align: center">
+						<li>
+							<a href="#">Home</a>
+						</li>
+						<li>
+							<a href="#">Áreas de Atuação</a>
+						</li>
+						<li>
+							<a href="#">Advogados</a>
+						</li>
+						<li>
+							<a href="#">Sobre Nós</a>
+						</li>
+						<li>
+							<a href="#">Contato</a>
+						</li>
+						
+					</ul>
+					
+				</div>
+				
+			</nav>
